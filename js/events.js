1cc4ec046556299a6385d9370d62f7a9fab015f5
Matthew McCormack <mmccormack1182@gmail.com>
2018-06-17 18:08:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,24 @@
-//define functions here
+function getIt() {
+  $(`p`).on(`click`,function() {
+    alert(`Hey!`);
+  }
+)}
 
+function frameIt() {
+  $(`.tasty`).on(load, function() {
+    
+  }
+  )
+ }
+ 
+ function pressIt() {
+   
+ }
+ 
+ function submitIt() {
+   
+ }
+ 
 $(document).ready(function(){
 
 // call functions here
